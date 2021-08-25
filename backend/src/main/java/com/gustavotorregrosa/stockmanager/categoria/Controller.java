89b9767a384143500e13e703284fe52b55259fe8
Gustavo <gustavo.torregrosa@gmail.com>
2021-08-25 15:19:40 -0300

    parcial

--- backend/src/main/java/com/gustavotorregrosa/stockmanager/categoria/Controller.java
+++ backend/src/main/java/com/gustavotorregrosa/stockmanager/categoria/Controller.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -21,26 +25,33 @@ public class Controller {
 	@Autowired
 	private Repository repository;
 	
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> tratativaErro(Exception e) {
+		return ResponseEntity.internalServerError().body(e.getClass().toString());
+		
+	}
+	
+	
 	@GetMapping
 	public ResponseEntity<List<Entity>> getAll(){
 		try {
 			List<Entity> listagem = this.repository.findAll();
 			return ResponseEntity.ok().body(listagem);
 		}catch(Exception e) {
-			return ResponseEntity.status(500).build();			
+			throw e;		
 		}
 		
 	}
 	
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<Entity> findById(@PathVariable String id){
-		System.out.println(id);
 		try {
 			Entity categoria = this.repository.getById(id);
 			System.out.println(categoria);
 			return ResponseEntity.ok().body(categoria);
 		}catch(Exception e) {
-			return ResponseEntity.status(500).build();			
+			throw e;	
 		}
 	}
 	
@@ -51,19 +62,32 @@ public class Controller {
 			Entity categoriaObj = this.repository.save(categoria);
 			return ResponseEntity.ok().body(categoriaObj);
 		}catch (Exception e) {
-			return ResponseEntity.status(500).build();		
+			throw e;
 		}
 	}
 	
+	@PutMapping(value  = "/{id}")
+	public ResponseEntity<Entity> update(@PathVariable String id, @RequestBody Entity categoria){
+		try {
+			Entity categoriaObj = this.repository.getById(id);
+			categoriaObj.setNome(categoria.getNome());
+			repository.save(categoriaObj);
+			return ResponseEntity.ok().body(categoriaObj);
+		}catch (Exception e) {
+			throw e;
+		}
+	}
 	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Entity> delete(@PathVariable String id){
+		try {
+			Entity categoriaObj = this.repository.getById(id);
+			this.repository.delete(categoriaObj);
+			return ResponseEntity.ok().body(categoriaObj);
+		}catch(Exception e) {
+			throw e;
+		}
+			
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
